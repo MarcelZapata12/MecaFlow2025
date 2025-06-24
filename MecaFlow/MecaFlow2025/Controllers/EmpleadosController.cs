66acad83cf4380dd023594260f73bf36a1f6b1c8
Marcel1203 <Mazaca120703@gmail.com>
2025-06-24 12:02:02 -0600

    Modulo de empleados (CRUD) y funcionamiento de registro de Asistencia de Empleados. Modulo clientes listo

---
+++ MecaFlow/MecaFlow2025/Controllers/EmpleadosController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MecaFlow2025.Models;
+
+namespace MecaFlow2025.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private readonly MecaFlowContext _context;
+
+        public EmpleadosController(MecaFlowContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Empleados
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Empleados.ToListAsync());
+        }
+
+        // GET: Empleados/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados
+                .FirstOrDefaultAsync(m => m.EmpleadoId == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleado);
+        }
+
+        // GET: Empleados/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Empleados/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EmpleadoId,Nombre,Cedula,Correo,Puesto,FechaIngreso,Activo,FechaRegistro")] Empleado empleado)
+        {
+            if (ModelState.IsValid)
+            {
+                // Establecer valores por defecto si no se proporcionan
+                if (empleado.Activo == null)
+                {
+                    empleado.Activo = true;
+                }
+                if (empleado.FechaRegistro == null)
+                {
+                    empleado.FechaRegistro = DateTime.Now;
+                }
+
+                _context.Add(empleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleados/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+            return View(empleado);
+        }
+
+        // POST: Empleados/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EmpleadoId,Nombre,Cedula,Correo,Puesto,FechaIngreso,Activo,FechaRegistro")] Empleado empleado)
+        {
+            if (id != empleado.EmpleadoId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(empleado);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EmpleadoExists(empleado.EmpleadoId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(empleado);
+        }
+
+        // GET: Empleados/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var empleado = await _context.Empleados
+                .FirstOrDefaultAsync(m => m.EmpleadoId == id);
+            if (empleado == null)
+            {
+                return NotFound();
+            }
+
+            return View(empleado);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var empleado = await _context.Empleados.FindAsync(id);
+            if (empleado != null)
+            {
+                _context.Empleados.Remove(empleado);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmpleadoExists(int id)
+        {
+            return _context.Empleados.Any(e => e.EmpleadoId == id);
+        }
+    }
+}
